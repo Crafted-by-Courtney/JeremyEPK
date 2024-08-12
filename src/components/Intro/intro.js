0bef672d650d36ed63104f321d8eb66d80f97910
Courtney Cook <courtneycook82@gmail.com>
2024-08-12 06:14:01 -0400

    Editing the Intro

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,26 +1,40 @@
-import React from 'react'
-import './intro.css';
-import kiki1 from '../images/kiki1.png';
-import btnImg from '../images/micr.png'
-import { Link } from 'react-scroll';
+import React from "react";
+import "./intro.css";
+import jeremyintronobg from "../images/jeremyintronobg.png";
+import { Link } from "react-scroll";
 
 const Intro = () => {
   return (
     <>
-    <section id="intro">
-      <div className='introContent'>
-        <span className='hello'>Hello,</span>
-        <span className='introText'>
-          I'm <span className='introName'><span>Kiki</span><br/></span> Music Artist
-        </span>
-        <p className='introPara'>I am a versatile, seasoned, music artist with a tapestry of experience in singing, songwriting, rapping, captivating live performances, playing piano, dance mastery, and choreographic brilliance.</p>
-        <Link><button className='btn'><img src={btnImg} alt='Hire me' className='btnImg'/> Hire Me</button></Link>
-      </div>
-      <img src={kiki1} alt="Profile" className='bg' />
-    </section>
-  </>
-  
-  )
-}
+      <section id="intro">
+        <div className="introContent">
+          <span className="hello">Hello,</span>
+          <span className="introText">
+            I'm{" "}
+            <span className="introName">
+              <span>Jeremy</span>
+              <br />
+            </span>{" "}
+            Jazz Pianist and Keyboardist
+          </span>
+          <p className="introPara">
+            An eclectic music artist with a unique blend of talents that
+            captivate audiences around the world. Hailing from Detroit, my
+            journey in the music industry began at a young age, fueled by a
+            passion for creativity and self-expression. My solo album, Hidden
+            Treasures, is an soulful mix of styles featuring collaborations with
+            talented musicians. I'm thrilled to finally showcase the full range
+            of my musical abilities and connect with audiences on a deeper
+            level. Thank you for joining me on this journey!
+          </p>
+          <Link>
+            <button className="btn"> Hire Me </button>
+          </Link>
+        </div>
+        <img src={jeremyintronobg} alt="Profile" className="bg" />
+      </section>
+    </>
+  );
+};
 
-export default Intro
+export default Intro;
